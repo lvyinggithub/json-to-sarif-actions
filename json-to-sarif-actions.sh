fe552bd599bd933aa828f001a0e5a62d46919338
rain <34671786+lvyinggithub@users.noreply.github.com>
2021-03-15 12:52:53 +0800

    Update json-to-sarif-actions.sh

--- json-to-sarif-actions.sh
+++ json-to-sarif-actions.sh
@@ -62,10 +62,10 @@ do
   echo $line_4_head $line_4_body
 
 
-  tiele=" \n|Package-string|Version-string|License Id string|License conditions string| \n"
-  content=" \n| Package-string|Version-string|License Id string|License conditions string"
-  #tiele= "\n$line_1_head" "|" "$line_2_head" "|" "$line_3_head" "|" "$line_4_head" "|"  "\n"
-  #content="\n"|" $line_1_body" "|" "$line_2_body" "|" "$line_3_body" "|" "$line_4_body" $content
+  #tiele=" \n|Package-string|Version-string|License Id string|License conditions string| \n"
+  #content=" \n| Package-string|Version-string|License Id string|License conditions string"
+  tiele= "\n$line_1_head" "|" "$line_2_head" "|" "$line_3_head" "|" "$line_4_head" "|"  "\n"
+  content="\n"|" $line_1_body" "|" "$line_2_body" "|" "$line_3_body" "|" "$line_4_body" $content
 done< ${json_file_path}
 
 
