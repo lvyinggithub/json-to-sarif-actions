14b471f3dedc2e0c2ad188a85bebae5de4d611ec
rain <34671786+lvyinggithub@users.noreply.github.com>
2021-03-15 11:20:44 +0800

    Update json-to-sarif-actions.sh

--- json-to-sarif-actions.sh
+++ json-to-sarif-actions.sh
@@ -33,25 +33,25 @@ do
   line_1=$(echo $line | awk -F'","' '{print $1}')
   echo $line_1 
   
-  line_1_head=$(echo $line_1 | awk -F':' '{print $1}' ) 
-  line_1_body=$(echo $line_1 | awk -F':' '{print $2}' ) 
+  line_1_head=$(echo $line_1 | awk -F'":' '{print $1}' ) 
+  line_1_body=$(echo $line_1 | awk -F'":' '{print $2}' ) 
   echo $line_1_head $line_1_body
   
   line_2=$(echo $line | awk -F'","' '{print $2}' ) 
-  line_2_head=$(echo $line_2 | awk -F':' '{print $1}' ) 
-  line_2_body=$(echo $line_2 | awk -F':' '{print $2}' ) 
+  line_2_head=$(echo $line_2 | awk -F'":' '{print $1}' ) 
+  line_2_body=$(echo $line_2 | awk -F'":' '{print $2}' ) 
   echo $line_2
   echo $line_2_head $line_2_body
   
   line_3=$(echo $line | awk -F'","' '{print $3}' ) 
-  line_3_head=$(echo $line_3 | awk -F':' '{print $1}' ) 
-  line_3_body=$(echo $line_3 | awk -F':' '{print $2}' ) 
+  line_3_head=$(echo $line_3 | awk -F'":' '{print $1}' ) 
+  line_3_body=$(echo $line_3 | awk -F'":' '{print $2}' ) 
   echo $line_3
   echo $line_3_head $line_3_body
   
   line_4=$(echo $line | awk -F'","' '{print $4}' ) 
-  line_4_head=$(echo $line_4 | awk -F':' '{print $1}' ) 
-  line_4_body=$(echo $line_4 | awk -F':' '{print $2}' ) 
+  line_4_head=$(echo $line_4 | awk -F'":' '{print $1}' ) 
+  line_4_body=$(echo $line_4 | awk -F'":' '{print $2}' ) 
   echo $line_4
   echo $line_4_head $line_4_body
 
