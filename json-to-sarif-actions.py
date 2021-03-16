0bf1ee8bde23dabdd03212a44c400f346a4ff6e3
rain <34671786+lvyinggithub@users.noreply.github.com>
2021-03-16 20:29:59 +0800

    Update json-to-sarif-actions.py

--- json-to-sarif-actions.py
+++ json-to-sarif-actions.py
@@ -93,4 +93,3 @@ sarif_file_content = '{ "$schema": "https://schemastore.azurewebsites.net/schema
 
 with open(licens_sarif_name, 'w') as file_object:
     file_object.write(sarif_file_content.strip())
-
